c6d0ef4c6388b8467a807ea17a0f91623dafd621
Luis Aragon <lmaragon@gmail.com>
2019-05-23 20:08:35 -0700

    commit

---
+++ scripts/util.R
@@ -0,0 +1,10 @@
+library("landscapemetrics")
+library("raster")
+library("rgdal")
+library("dplyr")
+
+raster.to.ESPG.3857 <- function(raster1){
+  mercatorCRS <- CRS("+init=epsg:3857")
+  newRaster <- projectRaster(raster1, crs=mercatorCRS, method = "ngb", res = 30, na.rm = TRUE)
+  return(newRaster)
+}
